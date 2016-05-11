bb6ac468a9e547564a1345655ec3177653e44ed9
forbesb <magnos.cartus@gmail.com>
2016-05-10 23:23:41 -0400

    added more functionality to the non-android classes

--- app/src/main/java/xyz/brianf/infosessionbingo/BingoBoard.java
+++ app/src/main/java/xyz/brianf/infosessionbingo/BingoBoard.java
@@ -1,5 +1,8 @@
 package xyz.brianf.infosessionbingo;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by brian on 09/05/16.
  */
@@ -7,6 +10,18 @@ public class BingoBoard {
     private BingoSquare[][] squares;
 
     public BingoBoard(){
+
+    }
+
+    public void initSquares(int sideLength){
+        squares = new BingoSquare[sideLength][sideLength];
+        ArrayList<String> RandomizedOptions = BingoOptions.getRandomizedOtions(sideLength*sideLength);
+        for (int i = 0; i < 5; i++){
+            for (int y = 0; y<5; y++ ){
+                BingoSquare newsquare = new BingoSquare(RandomizedOptions.get(y+sideLength*i));
+                squares[i][y] = newsquare;
+            }
+        }
 
     }
 
--- app/src/main/java/xyz/brianf/infosessionbingo/BingoOptions.java
+++ app/src/main/java/xyz/brianf/infosessionbingo/BingoOptions.java
@@ -1,11 +1,33 @@
 package xyz.brianf.infosessionbingo;
 
+import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by brian on 09/05/16.
  */
 public class BingoOptions {
     public static ArrayList<String> options;
+    private static void initOptionList(){
+        //for now, just letters.
+        for (char i = 'a'; i <= 'z'; i++){
+            options.add(String.valueOf(i));
+        }
+
+    }
+
+    public static ArrayList<String> getRandomizedOtions(int n){
+        if (options == null){
+            initOptionList();
+        }
+        Collections.shuffle(options);
+        ArrayList<String> RandomizedOptions = new ArrayList<>();
+        for (int i = 0; i<n; i++){
+            RandomizedOptions.add(options.get(i));
+        }
+        return RandomizedOptions;
+    }
 
 }
